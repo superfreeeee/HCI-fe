40fc91ec2db578a6b64f51207fec16460063b823
Chen YZ <906662721@qq.com>
2021-03-25 20:39:00 +0800

    feat: jenkinsfile

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,14 +4,9 @@
 NODE_LIST="39.97.124.144"
 #应用部署到的远程服务器目录
 REMOTE_DIR="/opt/apache-tomcat-9.0.36/webapps" 
-#需要部署的项目名称（需和maven的project名一样,多个用空格分开）
-#NEED_DEPLOY_PROJECT="user-server user-mgr info-mgr"
-NEED_DEPLOY_PROJECT="coin-web"
 # Date/Time Veriables
 LOG_DATE='date "+%Y-%m-%d"'
 LOG_TIME='date "+%H:%M:%S"' 
-CDATE=$(date "+%Y%m%d")
-CTIME=$(date "+%H%M%S") 
 #Shell Env
 TARGET_DIR="/var/jenkins_home/deploy"
 MOUNT_DIR="/data/jenkins_home/deploy"
@@ -19,18 +14,12 @@ SHELL_NAME="deploy.sh"
 SHELL_DIR="/deploy/log"
 SHELL_LOG="${SHELL_DIR}/${SHELL_NAME}.log" 
 #Code Env
-JAR_DIR="/deploy/jar"
-CONFIG_DIR="/deploy/config"
 LOCK_FILE="/tmp/deploy.lock"
-REMOTE_JAVA_HOME="/opt/jdk-14.0.1/bin" 
-WORKSPACE="../"
 usage(){
     echo  $"Usage: $0 [projectJarPath] [ deploy | rollback ]"
 } 
 init() {
     create_dir $SHELL_DIR;
-    create_dir $JAR_DIR;
-    create_dir $CONFIG_DIR;
 }
 create_dir() {
    if [ ! -d $1 ]; then  
@@ -50,7 +39,6 @@ write_log(){
 #拷贝应用的jar包到远程服务器
 scp_dist(){
     WORKSPACE=$1
-    SOURCE_DIR=${JAR_DIR}/${CDATE}${CTIME}
     write_log "Scp jar file to remote machine..."
     for node in $NODE_LIST;do
       scp -r ${WORKSPACE}/dist $node:${REMOTE_DIR}
@@ -61,11 +49,9 @@ scp_dist(){
 # 杀掉远程服务器上正在运行的项目
 cluster_node_remove(){
     write_log "Kill all runing project on the cluster..."
-    for project in $NEED_DEPLOY_PROJECT;do
-      for node in $NODE_LIST;do
+    for node in $NODE_LIST;do
         ssh $node "/opt/apache-tomcat-9.0.36/bin/shutdown.sh"
         ssh $node "rm -rf /opt/apache-tomcat-9.0.36/webapps/ROOT"
-      done
     done
 } 
 #在远程服务器上启动项目
