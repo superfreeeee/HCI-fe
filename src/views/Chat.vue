24efab76ac87ea326f98905252b5f5fddb671c7e
cclin <181250083@smail.nju.edu.cn>
2021-05-23 02:20:35 +0800

    add: responsive chatbox

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,46 +1,49 @@
 <template>
   <div class="box">
-    <el-row type="flex" justify="center">
-      <el-col>
-        <JwChat
-          :taleList="taleList"
-          @enter="bindEnter"
-          v-model="text"
-          :toolConfig="tool"
-        />
-      </el-col>
-    </el-row>
+    <div class="chat-header">co$in 小助手</div>
+    <JwChat
+      :taleList="taleList"
+      @enter="bindEnter"
+      v-model="text"
+      :toolConfig="tool"
+      :width="800"
+      :height="height"
+    />
   </div>
 </template>
 
 <script>
-import voiceRecognition from '../common/utils/voiceRecognition'
-const recorder = new voiceRecognition('en_us', 'mandarin', '5f27b6a9')
+// import voiceRecognition from '../common/utils/voiceRecognition'
+// const recorder = new voiceRecognition('en_us', 'mandarin', '5f27b6a9')
+import coinPng from '../assets/coin.png'
+import logoPng from '../assets/logo.png'
 
 export default {
   name: 'Chat',
   data() {
     return {
+      width: '70%',
+      height: '80vh',
       text: '',
       taleList: [
         {
-          date: '2020/04/25 21:19:07',
+          // date: '2020/04/25 21:19:07',
           text: { text: '起床不' },
           mine: false,
           name: 'coin小助手',
-          img: '../assets/coin.png',
+          img: coinPng,
         },
         {
-          date: '2020/04/16 21:19:07',
+          // date: '2020/04/16 21:19:07',
           text: { text: '我不饿' },
           mine: true,
           name: 'cclin',
-          img: '../assets/logo.png'
+          img: logoPng,
         },
       ],
       tool: {
         // file img video 现在只配置了三个图标
-        show: ['file', 'img'],
+        show: ['file', 'img', 'video'],
         callback: this.toolEvent,
       },
     }
@@ -52,6 +55,7 @@ export default {
     },
     bindEnter() {
       console.log('bind press enter')
+      console.log(this.text)
     },
     // onVoiceStart() {
     //   recorder.start()
@@ -67,12 +71,69 @@ export default {
 .box {
   width: 100%;
   height: 100%;
-  background-color: rgb(163, 177, 189);
+  background-color: hsl(207, 100%, 91%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.box .chat-box {
-  margin: 6% auto;
+.box .chat-header {
+  position: fixed;
+  top: 5%;
+  font-size: larger;
+  width: 200px;
+  text-align: center;
+  color: rgb(244, 153, 146);
+  background-color: #fdfcf8;
   border-radius: 20px;
-  padding: 10px;
+  box-shadow: 1px 1px 1px 1px slategrey;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+.chatPage[style] {
+  border-radius: 20px;
+  box-shadow: 1px 1px 1px 1px #454948;
+  margin: 5px;
+  background-color: #fdfcf8;
+}
+
+.web__msg-input[placeholder] {
+  background-color: #fffafa;
+}
+
+.wrapper .scroller .web__main-user img[data-v-422b11e3] {
+  border-radius: 0;
+}
+
+/* 超小型设备（电话，600px 及以下） */
+@media only screen and (max-width: 600px) {
+  .wrapper .scroller[style] {
+    width: 95vw;
+  }
+}
+
+/* 小型设备（纵向平板电脑和大型手机，600 像素及以上） */
+@media only screen and (min-width: 600px) {
+  .wrapper .scroller[style] {
+    width: 95vw;
+    padding-right: 0;
+  }
+}
+
+/* 中型设备（横向平板电脑，768 像素及以上） */
+@media only screen and (min-width: 768px) {
+  .wrapper .scroller[style] {
+    width: calc(100% - 2rem);
+    padding: 1rem;
+  }
+}
+
+/* 大型设备（笔记本电脑/台式机，992px 及以上） */
+@media only screen and (min-width: 992px) {
+}
+
+/* 超大型设备（大型笔记本电脑和台式机，1200px 及以上） */
+@media only screen and (min-width: 1200px) {
 }
 </style>
